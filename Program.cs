d21f250fc87d75ec254f1b3c194c68af5a0f5148
606zn <59501607+606zn@users.noreply.github.com>
2021-10-22 00:36:04 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Procent_vhojdeniy_simvola
+{
+    class Program
+    {
+        /// <summary>
+        /// Ввести строку произвольной длины и подсчитайте
+        /// процент вхождения заданного символа в строку.
+        /// </summary>
+        /// 
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string vhod;
+            Console.WriteLine("Введите строку: ");
+            vhod = Console.ReadLine();
+            char[] myArray = vhod.ToCharArray(); // вставить символы строки в массив
+            Console.Write("Введите искомый символ: ");
+            char x = char.Parse(Console.ReadLine());
+            int count = 0;
+            for (int i = 0; i < myArray.Length; i++) //поиск искомого символа в массиве
+                if (myArray[i] == x) count++;
+            int result = (100 * count) / vhod.Length;
+            Console.WriteLine("Процент совпадения искомого символа = " + result + "%");
+        }
+    }
+}
